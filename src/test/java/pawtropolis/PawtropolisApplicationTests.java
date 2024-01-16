f88c0e8db7930466ae3658f3011c63545cc91f10
Giacomo UGOLINI <giacomo.ugolini@alten.it>
2024-01-16 12:04:23 +0100

    refactor: move from kotlin to groovy; update project structure

---
+++ src/test/java/pawtropolis/PawtropolisApplicationTests.java
@@ -0,0 +1,13 @@
+package pawtropolis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawtropolisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
