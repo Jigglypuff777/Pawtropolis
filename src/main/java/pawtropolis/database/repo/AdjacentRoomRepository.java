84a9995f2742844673596cae646bdfb0cd8c0252
Alice Cintio <alicecintio0199@gmail.com>
2024-04-11 15:33:04 +0200

    removed: custom methods

--- src/main/java/pawtropolis/database/repo/AdjacentRoomRepository.java
+++ src/main/java/pawtropolis/database/repo/AdjacentRoomRepository.java
@@ -1,9 +1,16 @@
 package pawtropolis.database.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pawtropolis.database.entity.AdjacentRoomEntity;
 
+import java.util.List;
+
 @Repository
 public interface AdjacentRoomRepository extends JpaRepository<AdjacentRoomEntity, Long> {
+
+    @Query("SELECT ar FROM AdjacentRoomEntity ar JOIN FETCH ar.room JOIN FETCH ar.adjacentRoom WHERE ar.room.id = :roomId OR ar.adjacentRoom.id = :roomId")
+    List<AdjacentRoomEntity> findAdjacentsRooms(long roomId);
 }
+
--- src/main/java/pawtropolis/database/repo/DirectionRepository.java
+++ src/main/java/pawtropolis/database/repo/DirectionRepository.java
@@ -3,6 +3,9 @@ package pawtropolis.database.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pawtropolis.database.entity.DirectionEntity;
+import pawtropolis.game.gamecontroller.DirectionEnum;
+
 @Repository
 public interface DirectionRepository extends JpaRepository<DirectionEntity, Long> {
+    DirectionEnum findDirectionEnumById(long directionId);
 }
